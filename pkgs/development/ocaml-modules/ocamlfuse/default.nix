46a02a628e16de901eb483eded2ffe808e0cd4ae
Keith Collister <hnefatl@gmail.com>
2019-07-26 16:57:29 +0100

    Updated ocamlfuse to ~2.7.1_cvs6

--- pkgs/development/ocaml-modules/ocamlfuse/default.nix
+++ pkgs/development/ocaml-modules/ocamlfuse/default.nix
@@ -1,21 +1,23 @@
-{ stdenv, fetchFromGitHub, ocaml, camlidl, fuse, findlib }:
+{ stdenv, fetchFromGitHub, pkgconfig, dune, ocaml, camlidl, fuse, findlib }:
 
 stdenv.mkDerivation rec {
   name = "ocamlfuse-${version}";
-  version = "2.7.1_cvs5";
+  version = "2.7.1_cvs6";
 
   src = fetchFromGitHub {
     owner = "astrada";
     repo = "ocamlfuse";
-    rev = "v${version}";
-    sha256 = "01ayw2hzpxan95kncbxh9isj9g149cs8scq3xim1vy8bz085wb0m";
+    #rev = "v${version}";
+    # The release tagged 2.7.1_cvs6 is hard to build as it requires opam, a post-release patch introduced ocamlfind support as used by this builder
+    rev = "e35e76bee3b06806256b5bfca108b7697267cd5c";
+    sha256 = "1v9g0wh7rnjkrjrnw50145g6ry38plyjs8fq8w0nlzwizhf3qhff";
   };
 
-  buildInputs = [ocaml findlib];
-  propagatedBuildInputs = [camlidl fuse];
-  configurePhase = '' ocaml setup.ml -configure --prefix $out '';
-  buildPhase = "ocaml setup.ml -build";
-  installPhase = "ocaml setup.ml -install";
+  buildInputs = [ pkgconfig ocaml findlib dune ];
+  propagatedBuildInputs = [ camlidl fuse ];
+  buildPhase = "dune build @install";
+  # Setting the prefix causes dune to output libraries to lib/, rather than the "correct" location for ocamlfind
+  installPhase = "dune install --prefix $out --libdir $(ocamlfind printconf destdir)";
   createFindlibDestdir = true;
 
   meta = {
